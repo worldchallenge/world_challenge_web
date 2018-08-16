a9c996be599ce2c635e872754d7612171f9f296f
Chris Heckler <hecklerchris@hotmail.com>
2018-08-15 22:50:25 -0700

    Added templates folder and adjusted settings.py

--- world_challenge_web/views.py
+++ world_challenge_web/views.py
@@ -85,7 +85,7 @@ def activate(request, uidb64, token):
         user = User.objects.get(pk=uid)
     except:(TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
-    if user is not None and account_activation_token.check_token(usxer, token):
+    if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
         auth_login(request, user)
