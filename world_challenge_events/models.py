7fe75913803730acdf6caedbd65bbc1cc7052203
Chris Heckler <hecklerchris@hotmail.com>
2018-08-29 12:58:30 -0700

    Location,description added to models

--- world_challenge_events/models.py
+++ world_challenge_events/models.py
@@ -3,3 +3,5 @@ from django.db import models
 
 class Event(models.Model):
     name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    description = models.CharField(max_length=400)
