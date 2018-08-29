0000b614d6d4df719662a2e0b4bc76a059cc18f7
Chris Heckler <hecklerchris@hotmail.com>
2018-08-29 13:38:35 -0700

    Add helper function and field to model for user event

--- world_challenge_events/models.py
+++ world_challenge_events/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 
+def user_dir_path(instance, filename):
+    """Uploads user file to their directory"""
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
 class Event(models.Model):
+    """Creates an event and saves the user event to their profile dir"""
+
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
     description = models.CharField(max_length=400)
+    file_upload = models.FileField(upload_to=user_dir_path)
