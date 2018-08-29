4e368950e83c2fcd21d65f82a09e30b414b8b3d4
Chris Heckler <hecklerchris@hotmail.com>
2018-08-29 13:27:28 -0700

    CreateEventFormView created

--- world_challenge_events/views.py
+++ world_challenge_events/views.py
@@ -1,7 +1,10 @@
 from django.utils import timezone
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views import View
+from django.http import HttpResponseRedirect
 
+from .forms import CreateEventForm
 from .models import Event
 
 
@@ -14,3 +17,22 @@ class EventListView(ListView):
         context = super().get_context_data(**kwargs)
         context['now'] = timezone.now()
         return context
+
+
+class CreateEventFormView(View):
+    """This view handles app logic for event creation"""
+
+    form_class = CreateEventForm
+    template_name = "create_event.html"
+    initial = {'key': 'value'}
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/success/')
+
+        return render(request, self.template_name, {'form': form})
