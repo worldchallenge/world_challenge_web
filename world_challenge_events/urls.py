f1122fe99a68b0bbde49011ff1b47c8467a34820
Chris Heckler <hecklerchris@hotmail.com>
2018-08-29 13:02:54 -0700

    CreateEventView added

--- world_challenge_events/urls.py
+++ world_challenge_events/urls.py
@@ -4,4 +4,6 @@ from .views import EventListView
 
 urlpatterns = [
     path('', EventListView.as_view(), name='event-list'),
+    path('/events/create_event/', CreateEventView.as_view(),
+         name='create-event')
 ]
