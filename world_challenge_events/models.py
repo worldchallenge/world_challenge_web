1e9be294e3861b6f122b794c5e45c7e5d831a4ac
Chris Heckler <hecklerchris@hotmail.com>
2018-09-04 19:14:01 -0700

    Added uuid to model

--- world_challenge_events/models.py
+++ world_challenge_events/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+import uuid
 
 
 
 class Event(models.Model):
     """Creates an event and saves the user event to /var/www"""
 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255, blank=True)
     description = models.CharField(max_length=400, blank=True)
