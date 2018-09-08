fe3b9dae1a4072dbdd2a0da903e2c2bfd3a90d5d
chrisheckler <hecklerchris@hotmail.com>
2018-09-08 03:13:28 -0700

    View added for votes on object

--- world_challenge_events/views.py
+++ world_challenge_events/views.py
@@ -6,9 +6,15 @@ from django.views import View
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core import serializers
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.decorators import login_required
 
 from .forms import CreateEventForm
 from .models import Event
+from vote.models import Vote
+
+
+VOTE = (('up', 1), ('down', -1), ('clear', 0))
 
 
 class EventListView(ListView):
@@ -56,3 +62,39 @@ class EventDetailView(DetailView):
         return render(request, 'world_challenge_events/event_detail.html',
                       context={'event': event})
 
+
+class VoteView(View):
+    """Class manages voting"""
+
+    @login_required
+    def vote_on_object(request, app, object_id, vote):
+        """
+        Generic object vote function
+        """
+        
+        next = request.REQUEST.get('next', None)
+        if not next:
+            next = request.META.get('HTTP_REFERER', None)
+        if not next:
+            next = '/'
+        
+        print app
+        
+        try:
+            app_label, model = app.split('_')
+            ctype = ContentType.objects.get(app_label=app_label, model=model)
+        except ContentType.DoesNotExist:
+            return HttpResponseRedirect(next + '?error=app-not-exists')
+            
+        klass = ctype.model_class()
+        
+        try:
+            Vote.objects.record_vote(klass.objects.get(pk=object_id), request.user, dict(VOTE)[vote])
+        except klass.DoesNotExist:
+            return HttpResponseRedirect(next + '?error=object-not-exists')
+        except KeyError:
+            return HttpResponseRedirect(next + '?error=vote-not-valid')
+        except ValueError:
+            return HttpResponseRedirect(next + '?error=vote-not-valid')
+        
+        return HttpResponseRedirect(next + '?success=vote-registered')
